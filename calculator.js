2cb7c5944f62e23d0b78d9604b7455409fdd5fa7
tifff-wang <thiffanyjun@hotmail.com>
2023-07-12 21:59:52 +1200

    Fix bugs based on different cases

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
 const display = document.getElementById('display')
 const buttons = [...document.getElementsByClassName('button')]
-const operators = ['%', '/', '*', '-', '+', '=']
+const specials = ['%', '/', '*', '-', '+', '=']
+const operators = ['/', '*', '-', '+']
 let calculation = ''
+let output = ''
+let decimalDotCount = 0
+let isEquals = false
 
 buttons.forEach((button) => {
   button.addEventListener('click', (evt) => {
@@ -9,22 +13,43 @@ buttons.forEach((button) => {
 
     if ((input === '=') & (calculation !== '')) {
       let result = eval(calculation)
-      calculation = Number.isInteger(result) ? result : result.toFixed(5)
+      output = Number.isInteger(result) ? result : parseFloat(result).toFixed(5)
       displayCalculation()
+      isEquals = true
     } else if (input === '%') {
       input = '/100'
       updateCalculation(input)
     } else if (input === 'AC') {
+      output = ''
       calculation = ''
       displayCalculation()
     } else if (input === 'DE') {
-      calculation = calculation.toString().slice(0, -1)
+      output = output.toString().slice(0, -1)
+      calculation = calculation.slice(0, -1)
       displayCalculation()
     } else if (input === '.') {
-      input = calculation ? '.' : '0.'
+      input = output ? '.' : '0.'
+      decimalDotCount++
+      if (decimalDotCount > 1) {
+        return
+      }
       updateCalculation(input)
-    } else if (calculation === '' && operators.includes(input)) {
+    } else if (calculation === '' && specials.includes(input)) {
       return
+    } else if (operators.includes(input)) {
+      isEquals = false
+      let lastInput = calculation.slice(-1)
+      if (operators.includes(lastInput)) {
+        calculation.slice(0, -1)
+      }
+      calculation += input
+      output = ''
+      display.innerText = input
+    } else if (isEquals) {
+      output = ''
+      calculation = ''
+      updateCalculation(input)
+      isEquals = false
     } else {
       updateCalculation(input)
     }
@@ -32,10 +57,11 @@ buttons.forEach((button) => {
 })
 
 function updateCalculation(input) {
+  output += input
   calculation += input
   displayCalculation()
 }
 
 function displayCalculation() {
-  display.innerText = calculation
+  display.innerText = output
 }
